820269c929071b85e277e1060af6392b153813cc
sayedosama7 <sayedosama088@gmail.com>
2023-01-08 23:15:15 +0200

    n

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from 'react'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import  './App.css'
 import Home from './Components/Home/Home'
+import {useState , useEffect} from 'react'
+import DotLoader from "react-spinners/DotLoader";
 
 const App = () => {
+  const [loading,setLoading] =useState([false]);
+  useEffect(()=>{
+  setLoading(true)
+  setTimeout(()=>{
+  setLoading(false)
+  } , 3000)
+
+} ,[])
   return (
     <>
     <BrowserRouter>
+    {loading ? 
+    <div className="loader">
+    <DotLoader color={'#dcca87'}  size={90} speedMultiplier={1} />
+    </div>:
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
+}
     </BrowserRouter>
     
     
